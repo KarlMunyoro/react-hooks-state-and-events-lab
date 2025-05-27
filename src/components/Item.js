e9934cbe7c23ac726ead213e21dc4cd22179ae98
Karl Munyoro Kamau <kxrlmunyoro@gmail.com>
2025-05-27 09:31:24 +0300

    React UseState Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [cart, setCart] = useState(false)
+  const appclass = cart? "in-cart":""
+
   return (
-    <li className="">
+    <li className={appclass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => setCart(x => !x)}>{cart? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
